a853805d7ecdb1dfcf58c42acf7f9b5ed5fa07f6
edoapra <edoardo.apra@gmail.com>
2017-12-13 17:22:26 -0800

    use my fork of scalapack that has the gcc7 fix

--- travis/build_scalapack.sh
+++ travis/build_scalapack.sh
@@ -3,8 +3,8 @@ source $TRAVIS_BUILD_DIR/travis/nwchem.bashrc
 if [[ "$USE_64TO32" != "y" ]] ; then exit 0; fi
 cd $TRAVIS_BUILD_DIR
 os=`uname`
-git clone https://github.com/scibuilder/scalapack.git
-#svn co --non-interactive --trust-server-cert https://icl.utk.edu/svn/scalapack-dev/scalapack/trunk/ scalapack
+#git clone https://github.com/scibuilder/scalapack.git
+git clone https://github.com/edoapra/scalapack.git 
 mkdir -p scalapack/build
 cd scalapack/build
 cmake -Wno-dev ../ -DCMAKE_BUILD_TYPE=RelWithDebInfo -DTEST_SCALAPACK=OFF  -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON  -DBLAS_blas_LIBRARY="-L$TRAVIS_BUILD_DIR -lopenblas" -DLAPACK_lapack_LIBRARY="-L$TRAVIS_BUILD_DIR  -lopenblas"  -DCMAKE_INSTALL_PREFIX=$TRAVIS_BUILD_DIR
