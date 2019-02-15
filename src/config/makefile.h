69e28f4222dad65d5994921b0a21efe624ece274
Xiguo Xie <xiexiguo@live.com>
2019-02-15 11:50:09 +0800

    Fixed compiling errors on Nvidia GPU

--- src/config/makefile.h
+++ src/config/makefile.h
@@ -2862,6 +2862,7 @@ ifndef CUDA
   CUDA = nvcc
 endif
 ifdef TCE_CUDA
+  DEFINES += -DTCE_CUDA
   CORE_LIBS += $(CUDA_LIBS)
   EXTRA_LIBS += -lstdc++
   ifdef USE_TTLG
@@ -2876,6 +2877,7 @@ ifndef HIP
   HIP = hipcc
 endif
 ifdef TCE_HIP
+  DEFINES += -DTCE_HIP
   CORE_LIBS += $(HIP_LIBS)
   EXTRA_LIBS += -lstdc++
 endif
@@ -3007,10 +3009,10 @@ CUDA_VERS_GE8=$(shell nvcc --version|egrep rel|  awk '/release 9/ {print "Y";exi
               CUDA_FLAGS = -O3  -std=c++11 -DNOHTIME -Xptxas --warn-on-spills $(CUDA_ARCH) 
         endif
 (%.o):  %.cu
-	$(CUDA) -c $(CUDA_FLAGS) $(CUDA_INCLUDE) -I$(NWCHEM_TOP)/src/tce/ttlg/includes -o $% $<
+	$(CUDA) -c -DTCE_CUDA $(CUDA_FLAGS) $(CUDA_INCLUDE) -I$(NWCHEM_TOP)/src/tce/ttlg/includes -o $% $<
 else
 (%.o):  %.cu
-	$(CUDA) -c $(CUDA_FLAGS) $(CUDA_INCLUDE) -o $% $<
+	$(CUDA) -c -DTCE_CUDA $(CUDA_FLAGS) $(CUDA_INCLUDE) -o $% $<
 endif
 endif
 
